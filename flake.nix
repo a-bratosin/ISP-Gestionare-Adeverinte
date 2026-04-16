f341148b1ca0286e44ed9d889f425a3915080bef
Balas Vlad-George <vlad_george.balas@stud.acs.upb.ro>
2026-04-16 22:22:24 +0300

    dev env pentru NixOS

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "ISP-Gestionare-Adeverinte dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        jdk = pkgs.zulu25;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            zulu25
+            pkgs."go-task"
+          ];
+
+          shellHook = ''
+            export JAVA_HOME=${jdk}
+          '';
+        };
+      });
+}
