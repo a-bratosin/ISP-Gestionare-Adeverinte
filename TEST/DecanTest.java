f3ae8d77e147bd2ea84eef2647e3e03ff072ed1f
patraxbi <patraxbi@outlook.com>
2026-05-06 16:51:59 +0300

    junitTest worked

---
+++ TEST/DecanTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DecanTest {
+    @Test
+    public void testSemneazaAdeverinta() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testSetSemnatura() {
+        assertTrue(true);
+        assertFalse(false);
+    }
+}
